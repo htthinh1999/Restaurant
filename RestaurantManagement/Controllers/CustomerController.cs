8190fe3fae5bbf7bf05f63e25d21f4b2b0809603
Huỳnh Tấn Thịnh <47586956+htthinh1999@users.noreply.github.com>
2021-09-23 00:34:03 +0700

    Init Project

---
+++ RestaurantManagement/Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantManagement.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Info()
+        {
+            return View();
+        }
+    }
+}
