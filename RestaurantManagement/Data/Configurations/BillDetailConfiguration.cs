b97a52c99c39d3496584aee9abd567dd8f5e490a
Huỳnh Tấn Thịnh <47586956+htthinh1999@users.noreply.github.com>
2021-10-04 22:16:53 +0700

    Update database (#20)
    * Init Project
    
    * Update connection string in appsettings.json
    
    * Add default connection for SQL Server online
    
    * - Create table Customer and table Table.
    - Change appsetting.json to support online and local database.
    - Remove unused code.
    
    * Update RestaurantDbContextFactory.cs
    
    * Change connection to online
    
    * Create Food table (#4)
    
    * ORDER_TABLE
    
    * Change database construction
    
    * Create BillDetail table
    
    * fix database (#18)
    
    * complete database
    
    * Replace BillDetail to BillDetails
    
    Co-authored-by: thinhtanhuynh <thinh.tanhuynh@infodation.vn>
    Co-authored-by: HienNguyen120201 <90005023+HienNguyen120201@users.noreply.github.com>
    Co-authored-by: loctran1002 <91211609+loctran1002@users.noreply.github.com>
    Co-authored-by: binhbuibksg0123 <74866715+binhbuibksg0123@users.noreply.github.com>
    Co-authored-by: pmk2k1 <khanhphung1801@gmail.com>
    Co-authored-by: ThuongNguyenVan158 <80138135+ThuongNguyenVan158@users.noreply.github.com>
    Co-authored-by: Huỳnh Tấn Thịnh <thinhtanhuynh@infodation.vn>

--- RestaurantManagement/Data/Configurations/BillDetailConfiguration.cs
+++ RestaurantManagement/Data/Configurations/BillDetailConfiguration.cs
@@ -12,16 +12,14 @@ namespace RestaurantManagement.Data.Configurations
     {
         public void Configure(EntityTypeBuilder<BillDetail> builder)
         {
-            builder.ToTable("BILLDETAIL");
-            builder.HasKey(bd => bd.BillID);
-            builder.HasKey(bd => bd.FoodID);
+            builder.ToTable("BILL_DETAIL");
+            builder.HasKey(bd => new { bd.BillId, bd.FoodId });
             builder.Property(bd => bd.UnitPrice).IsRequired();
             builder.Property(bd => bd.Quantity).IsRequired();
             builder.Property(bd => bd.Price).IsRequired();
 
-            builder.HasOne(bd => bd.Bill).WithMany(k => k.BillDetail).HasForeignKey(bd => bd.BillID);
-            builder.HasOne(bd => bd.Food).WithMany(k => k.BillDetail).HasForeignKey(bd => bd.FoodID);
-            
+            builder.HasOne(bd => bd.Bill).WithMany(k => k.BillDetails).HasForeignKey(bd => bd.BillId);
+            builder.HasOne(bd => bd.Food).WithMany(k => k.BillDetails).HasForeignKey(bd => bd.FoodId);
         }
     }
 }
---
+++ RestaurantManagement/Migrations/20210929173444_CreateBillDetailTable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestaurantManagement.Migrations
+{
+    public partial class CreateBillDetailTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BILL_DETAIL",
+                columns: table => new
+                {
+                    BillId = table.Column<Guid>(nullable: false),
+                    FoodId = table.Column<int>(nullable: false),
+                    UnitPrice = table.Column<int>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false),
+                    Price = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BILL_DETAIL", x => new { x.BillId, x.FoodId });
+                    table.ForeignKey(
+                        name: "FK_BILL_DETAIL_BILL_BillId",
+                        column: x => x.BillId,
+                        principalTable: "BILL",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BILL_DETAIL_FOOD_FoodId",
+                        column: x => x.FoodId,
+                        principalTable: "FOOD",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BILL_DETAIL_FoodId",
+                table: "BILL_DETAIL",
+                column: "FoodId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BILL_DETAIL");
+        }
+    }
+}
