0a80963eb09ef6893fcb8d5710af8c6fbf7c333c
Nguyễn Văn Thương <80138135+ThuongNguyenVan158@users.noreply.github.com>
2021-10-18 23:00:10 +0700

    create full feature order-table (#26)

---
+++ RestaurantManagement/Models/TableHistoryViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Models
+{
+    public class TableHistoryViewModels
+    {
+        public int Id { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public string TableName { get; set; }
+        public int PeopleCount { get; set; }
+
+    }
+}
