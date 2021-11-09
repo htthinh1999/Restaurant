683e1b44a59d361c8ecb84fe4881cfc3c70e0a16
Huỳnh Tấn Thịnh <47586956+htthinh1999@users.noreply.github.com>
2021-11-10 03:56:33 +0700

    Add feature send mail to reset password and change language to Vietnamese (#41)

---
+++ RestaurantManagement/Models/ResetPasswordViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace RestaurantManagement.Models
+{
+    public class ResetPasswordViewModel
+    {
+        public string NewPassword { get; set; }
+        public string ConfirmNewPassword { get; set; }
+    }
+}
