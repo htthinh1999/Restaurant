29795b82581af754638df3d711d5dc1a11f52c05
Huỳnh Tấn Thịnh <47586956+htthinh1999@users.noreply.github.com>
2021-11-15 00:43:49 +0700

    Update Program.cs

--- RestaurantManagement/Program.cs
+++ RestaurantManagement/Program.cs
@@ -13,7 +13,7 @@ namespace RestaurantManagement
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run()
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
